9cf0de198af62527427ed1519ed1d8ac57f581bf
tiziano093 <t.marcozzi@outlook.com>
2025-02-03 22:09:59 +0100

    feat: n

--- graphql-server/config/partials.js
+++ graphql-server/config/partials.js
@@ -4,10 +4,11 @@ const {
   helloWorldResolvers,
 } = require("@tiziano093/graphql-hello-world");
 const { postTypeDefs, postResolvers } = require("@tiziano093/graphql-post");
+const { userTypeDefs, userResolvers } = require("@tiziano093/graphql-user");
 
 const schema = makeExecutableSchema({
-  typeDefs: [helloWorldTypeDefs, postTypeDefs],
-  resolvers: [helloWorldResolvers, postResolvers],
+  typeDefs: [helloWorldTypeDefs, postTypeDefs, userTypeDefs],
+  resolvers: [helloWorldResolvers, postResolvers, userResolvers],
 });
 
 module.exports = schema;
